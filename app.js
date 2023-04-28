cd68c5139079d5e8dccd21461bd3caf6d323789d
Yaser Hajian <mohammadyaser1379@gmail.com>
2023-04-28 13:01:52 +0330

    mongoose connection added

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const dotenv = require('dotenv')
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
-dotenv.config()
+dotenv.config();
+
+mongoose
+  .connect(process.env.mongoURl)
+  .then(() => console.log("connected to database"))
+  .catch((error) => console.log("could not connect to database"));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT , ()=>{
+app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
-})
+});
