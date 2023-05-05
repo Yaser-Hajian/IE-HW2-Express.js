a47b72799069ad609739f1ae9c08ef5a46c6915a
Yaser Hajian <mohammadyaser1379@gmail.com>
2023-05-05 11:31:42 +0330

    validation function added

---
+++ src/routers/controller.js
@@ -0,0 +1,25 @@
+const { validationResult } = require("express-validator");
+
+module.exports = class {
+  validationBody(req, res) {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      const errors = result.array();
+      const messages = [];
+      errors.forEach((err) => messages.push(err.msg));
+      res.status(400).json({
+        data: messages,
+        message: "validation error",
+      });
+      return false;
+    }
+    return true;
+  }
+  //middleware for validation
+  validate(req, res, next) {
+    if (!this.validationBody(req, res)) {
+      return;
+    }
+    next();
+  }
+};
