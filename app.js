1f277c5b7bc14b9578181790df6fb724edf8c387
Yaser Hajian <mohammadyaser1379@gmail.com>
2023-05-09 17:33:03 +0330

    router added

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
+const router = require("./src/routers/router")
 const app = express();
 dotenv.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.mongoURl)
   .then(() => console.log("connected to database"))
   .catch((error) => console.log("could not connect to database"));
 
+
+app.use("/" , router) 
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
+
