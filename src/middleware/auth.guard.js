041f7028fc80cca132c95265461fb8149b4e5734
Yaser Hajian <mohammadyaser1379@gmail.com>
2023-05-11 12:51:50 +0330

    add split to read token

--- src/middleware/auth.guard.js
+++ src/middleware/auth.guard.js
@@ -2,12 +2,11 @@ const jwt = require('jsonwebtoken');
 const User = require('./../models/user');
 
 const authGuard = async (req, res, next) => {
-  const token = req.header('Authorization')?.replace('Bearer ', '');
+  const token = req.header('Authorization')?.split(" ")[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findOne({ _id: decoded.id });
-
     if (!user) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
