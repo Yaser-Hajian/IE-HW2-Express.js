ac857eaa9338afa3835785b10730361289310903
Yaser Hajian <mohammadyaser1379@gmail.com>
2023-05-11 11:19:44 +0330

    authGuard added to routes

--- src/routers/admin/admin.router.js
+++ src/routers/admin/admin.router.js
@@ -2,28 +2,34 @@ const express = require("express");
 const router = express.Router();
 const adminValidator = require("./admin.validator");
 const adminController = require("./admin.controller");
+const authGuard = require("../../middleware/auth.guard");
 router.post(
   "/Professor",
+  authGuard,
   adminValidator.createProfessorValidator(),
   adminController.validate,
   adminController.createProfessor.bind(adminController)
 );
 router.get(
   "/Professors",
+  authGuard,
   adminController.findAllProfessors.bind(adminController)
 );
 
 router.get(
   "/Professor/:id",
+  authGuard,
   adminController.findProfessorById.bind(adminController)
 );
 
 router.delete(
   "/Professor/:id",
+  authGuard,
   adminController.deleteProfessorById.bind(adminController)
 );
 router.put(
   "/Professor/:id",
+  authGuard,
   adminValidator.updateProfessorValidator(),
   adminController.validate,
   adminController.updateProfessorById.bind(adminController)
@@ -31,6 +37,7 @@ router.put(
 
 router.post(
   "/student",
+  authGuard,
   adminValidator.createStudentValidator(),
   adminController.validate,
   adminController.createStudent.bind(adminController)
@@ -38,44 +45,62 @@ router.post(
 
 router.get(
   "/students",
- adminController.findAllStudents.bind(adminController));
+  authGuard,
+  adminController.findAllStudents.bind(adminController)
+);
 
 router.get(
   "/student/:id",
-  adminController.findStudentById.bind(adminController));
+  authGuard,
+  adminController.findStudentById.bind(adminController)
+);
 
 router.put(
   "/student/:id",
+  authGuard,
   adminValidator.updateStudentValidator(),
   adminController.validate,
-  adminController.updateStudentByID.bind(adminController));
+  adminController.updateStudentByID.bind(adminController)
+);
 
 router.delete(
   "/student/:id",
-   adminController.deleteStudentByID.bind(adminController));
+  authGuard,
+  adminController.deleteStudentByID.bind(adminController)
+);
 
 router.post(
   "/manager",
+  authGuard,
   adminValidator.createManagerValidator(),
   adminController.validate,
-  adminController.createManager.bind(adminController));
-  
+  adminController.createManager.bind(adminController)
+);
+
 router.get(
   "/managers",
-  adminController.getAllManagers.bind(adminController));
+  authGuard,
+  adminController.getAllManagers.bind(adminController)
+);
 
 router.get(
   "/manager/:id",
-  adminController.findManagerByID.bind(adminController));
-  
+  authGuard,
+  adminController.findManagerByID.bind(adminController)
+);
+
 router.delete(
   "/manager/:id",
-  adminController.deleteManagerById.bind(adminController));  
+  authGuard,
+  adminController.deleteManagerById.bind(adminController)
+);
 
-router.put("/manager/:id",
+router.put(
+  "/manager/:id",
+  authGuard,
   adminValidator.updateManagerValidator(),
   adminController.validate,
   adminController.updateManagerById.bind(adminController)
-  )
+);
 
 module.exports = router;
