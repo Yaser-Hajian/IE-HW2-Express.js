bbc267650132a324685f97b1bedc3c51e40e2e68
Yaser Hajian <mohammadyaser1379@gmail.com>
2023-05-11 12:57:12 +0330

    admin routes have authGuard and rollGuard

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authRouter = require("./auth/auth.router")
-const adminRouter = require("./admin/admin.router")
+const authRouter = require("./auth/auth.router");
+const adminRouter = require("./admin/admin.router");
+const authGuard = require("./../middleware/auth.guard")
+const rollGuard = require("./../middleware/roll.guard")
 
-router.use('/auth' , authRouter);
-router.use('/admin' , adminRouter)
+
+router.use("/auth", authRouter);
+router.use("/admin", authGuard, rollGuard("ItManager"), adminRouter);
 
 module.exports = router;
