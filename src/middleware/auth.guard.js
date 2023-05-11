4eb4b1c579899fd458560d3ee8f1e2e736cedf50
Yaser Hajian <mohammadyaser1379@gmail.com>
2023-05-11 11:18:24 +0330

    authGuard added

---
+++ src/middleware/auth.guard.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const User = require('./../models/user');
+
+const authGuard = async (req, res, next) => {
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({ _id: decoded.id });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+};
+
+module.exports = authGuard;
