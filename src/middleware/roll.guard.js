e47c3cdb7131d4dcbc67f9ba0ef34fad88e46452
Yaser Hajian <mohammadyaser1379@gmail.com>
2023-05-11 12:52:05 +0330

    rollGuard added

---
+++ src/middleware/roll.guard.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+const User = require('./../models/user');
+
+const rollGuard = (allowedRoles) => async (req, res, next) => {
+  const token = req.header('Authorization')?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findOne({ _id: decoded.id });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    if (!allowedRoles.includes(user.userType)) {
+      return res.status(403).json({ error: 'Forbidden' });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+};
+
+module.exports = rollGuard;
