7883187875639c77574b4546236ee62e91135387
Yaser Hajian <mohammadyaser1379@gmail.com>
2023-05-14 17:58:26 +0330

    some changes in fields

--- src/models/official_course.js
+++ src/models/official_course.js
@@ -1,30 +1,39 @@
 const mongoose = require("mongoose");
 const timestamps = require("mongoose-timestamp");
 
-const officialCourseSchema = mongoose.Schema({
-  course_name: {
-    type: String,
-    required: true,
-  },
-  major: {
-    type: String,
-    required: true,
-  },
-  course_prerequisites: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'OfficialCourse',
-    default: []
-  }],
-  course_requirements: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'OfficialCourse',
-    default: []
-  }],
-  credit: {
-    type: Number,
-    required: true
+const officialCourseSchema = mongoose.Schema(
+  {
+    course_id:{
+      type: Number,
+      required: true,
+      unique:true
+    },
+    course_name: {
+      type: String,
+      required: true,
+    },
+    major: {
+      type: String,
+      required: true,
+    },
+    course_prerequisites: [
+      {
+        type: String,
+        default: []
+      },
+    ],
+    course_requirements: [
+      {
+        type: String,
+        default: []
+      },
+    ],
+    credit: {
+      type: Number,
+      required: true,
+    },
   }
-});
+);
 
 officialCourseSchema.plugin(timestamps);
 const OfficialCourse = mongoose.model("OfficialCourse", officialCourseSchema);
