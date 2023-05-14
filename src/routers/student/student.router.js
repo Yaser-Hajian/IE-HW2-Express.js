334a14ba8de986bf9afe869671ea478f53fbb72f
Yaser Hajian <mohammadyaser1379@gmail.com>
2023-05-14 19:59:21 +0330

    a new route added

---
+++ src/routers/student/student.router.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const studentController = require("./student.controller");
+
+const router = express.Router();
+
+
+
+router.get("",studentController.getCourses.bind(studentController))
+
+module.exports = router;
